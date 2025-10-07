5abdc5335f6b1d112c76952dbce6fa9f7f3a2524
Atharv Darunkar <atharvdarunkar111@gmail.com>
2025-10-07 14:51:25 +0530

    feat:added composite dp

--- src/main/java/com/darunkar/design_patterns/ComposeFileSystemDemo.java
+++ src/main/java/com/darunkar/design_patterns/ComposeFileSystemDemo.java
@@ -1,12 +1,16 @@
 package com.darunkar.design_patterns;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 //Component Interface ( common for both file and folder)
 interface FileComponent{
     void showDetails(String indent);
     int getSize();
+    FileComponent search(String name);
+    int countFiles();
+    boolean delete(String name);
 }
 
 // implementation of file
@@ -26,6 +30,25 @@ class FileLeaf implements FileComponent{
     @Override
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public FileComponent search(String name) {
+        return this.name.equals(name) ? this : null;
+    }
+
+    @Override
+    public int countFiles() {
+        return 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean delete(String name) {
+        return false;
     }
 }
 
@@ -61,6 +84,52 @@ class FolderComposite implements FileComponent{
     public void remove(FileComponent fileComponent){
         children.remove(fileComponent);
     }
+
+    public FileComponent search(String name) {
+
+        if(this.name.equals(name)) return this;
+        for(FileComponent child: children){
+            FileComponent found = child.search(name);
+            if(found != null) return found;
+        }
+        return null;
+    }
+
+    @Override
+    public int countFiles() {
+        int count = 0;
+        for(FileComponent child : children){
+            count+=1;
+        }
+        return count;
+    }
+
+    @Override
+    public boolean delete(String name) {
+
+        Iterator<FileComponent> it = children.iterator();
+
+        while(it.hasNext()){
+            FileComponent child = it.next();
+
+            if(child instanceof FileLeaf file && file.getName().equals(name)){
+                it.remove();
+                System.out.println("File is deleted ! " +  name);
+                return true;
+            }else if(child instanceof FolderComposite folder){
+                if(folder.name.equals(name)){
+                    it.remove();
+                    System.out.println("Folder is deleted ! " +  name);
+                    return true;
+                }else if(folder.delete(name)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
 }
 
 
@@ -87,7 +156,21 @@ public class ComposeFileSystemDemo {
         rootFolder.add(new FileLeaf("file10", 10));
         rootFolder.showDetails("");
 
+        String seachFile="file1";
+        FileComponent foundFile=rootFolder.search(seachFile);
+        System.out.println();
+        System.out.println(foundFile != null ? seachFile + " Found" : "Not Found");
+
         System.out.println("\nSize of root folder " + rootFolder.getSize());
+
+        rootFolder.delete("Images");
+        rootFolder.delete("file2");
+
+        rootFolder.showDetails("");
+        rootFolder.countFiles();
+        System.out.println("\nSize of root folder " + rootFolder.getSize());
+
+
 
     }
 }
